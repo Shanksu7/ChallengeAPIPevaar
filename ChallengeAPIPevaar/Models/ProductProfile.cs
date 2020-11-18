91d1346209c8ec310322cdba91d5ee85f3083589
Shanks97 <juanp_cm97@hotmail.com>
2020-11-18 09:08:39 -0500

    AutoMapper

---
+++ ChallengeAPIPevaar/Models/ProductProfile.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using ChallengeDataObjects.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChallengeAPIPevaar.Models
+{
+    public class ProductProfile : Profile
+    {
+        public ProductProfile()
+        {
+            CreateMap<Product, ProductDetailModel>()
+            .ForMember(dest =>
+               dest.Type,
+                opt => opt.MapFrom(src => src.TypeNavigation.Name.ToString()));
+
+            CreateMap<ProductEntryModel, Product>()
+            .ForMember(dest =>
+               dest.Type,
+                opt => opt.MapFrom(src => (int) src.Type));
+
+        }
+    }
+}
